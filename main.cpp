1cd2a359db62225ea2926ab70324548c292cc339
Melvin Tomezak <92721333+MelvinTomezak@users.noreply.github.com>
2023-10-19 17:25:12 +0200

    Ajout des différents commentaire
    résoudre les problèmes restants.

--- main.cpp
+++ main.cpp
@@ -6,10 +6,11 @@
 
 using namespace std;
 
-int n = 5;
-int patients = 0;
-int examinedPatients = 0;
+int n = 5; // nombre de siége disponible
+int patients = 0; // on initie le nombre de patient a 0
+int examinedPatients = 0; //on initie le nombre de patient examiner a 0
 
+// on déclare nos différents sémaphore
 sem_t mutex_sem;
 sem_t patient_sem;
 sem_t medecin_sem;
@@ -18,8 +19,12 @@ sem_t medecinDone_sem;
 sem_t allDone_sem;
 sem_t sleepingMedecin_sem;
 
+// La fonction semaphores_init nous permet d'initier nos différents semaphores.
+// Si la valeur = 0 alors on partage la valeur uniquement entre les threads d'un même processus
+// Si la valeur = 1 c'est qu'on a un mutex
+// Un mutex = objet utilisé pour garantir qu'un thread puisse éxecuter une partie du code.
 void semaphores_init() {
-    sem_init(&mutex_sem, 0, 1);
+    sem_init(&mutex_sem, 0, 1); // synchronisation de threads, 1 indique qu'il est disponible
     sem_init(&patient_sem, 0, 0);
     sem_init(&medecin_sem, 0, 0);
     sem_init(&patientDone_sem, 0, 0);
@@ -27,7 +32,8 @@ void semaphores_init() {
     sem_init(&allDone_sem, 0, 0);
     sem_init(&sleepingMedecin_sem, 0, 0);
 }
-
+// La fonction semaphores_destroy, nous permet de détruire les sémaphores qu'on a utilisé précedement.
+// Cela permet de libérer les ressources associé au sémaphore précedement initié.
 void semaphores_destroy() {
     sem_destroy(&mutex_sem);
     sem_destroy(&patient_sem);
@@ -37,7 +43,9 @@ void semaphores_destroy() {
     sem_destroy(&allDone_sem);
 }
 
-void consultation(int patientId) {
+// La fonction attente nous permets de faire en sorte qu'un patient parte s'il y a pas de chaise disponible
+// Ou de le faire entrer si une chaise est disponible
+void attente(int patientId) {
     sem_wait(&mutex_sem);
 
     if (patients == n) {
@@ -53,8 +61,9 @@ void consultation(int patientId) {
     sem_post(&mutex_sem);
 }
 
-
-void examiner() {
+// La fonction consultation permet au médecin de prendre un patient en consultation pendant une durée détérminé.
+// Et de limiter le nombre de patient souhaiter pour simuler une journée de travail.
+void consultation() {
     while (true) {
         sem_wait(&patient_sem);
 
@@ -80,13 +89,13 @@ void examiner() {
 int main() {
     semaphores_init();
 
-    thread medecinThread(examiner);
+    thread medecinThread(consultation);
 
     vector<thread> patientThreads;
     int patientId = 1;
 
     while (patientId <= 10) {
-        patientThreads.push_back(thread(consultation, patientId));
+        patientThreads.push_back(thread(attente, patientId));
         patientId++;
         this_thread::sleep_for(chrono::milliseconds(400));
     }
